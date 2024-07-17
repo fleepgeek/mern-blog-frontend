1466aa2599c0a51b658f57ccd023c98c14a86edb
Emmanuel Okiche <fleepgeek@gmail.com>
2024-07-17 03:01:27 +0100

    click image to upload

--- src/forms/UploadImageForm.tsx
+++ src/forms/UploadImageForm.tsx
@@ -8,10 +8,9 @@ import {
   FormItem,
   FormMessage,
 } from "../components/ui/form";
+import { useCallback, useRef } from "react";
+import { FileUp, ImagePlus } from "lucide-react";
 import { Input } from "../components/ui/input";
-import { Button } from "../components/ui/button";
-import LoadingButton from "../components/LoadingButton";
-// import { useEffect } from "react";
 
 const formSchema = z.object({
   imageFile: z.instanceof(File, { message: "imageFile required" }),
@@ -31,6 +30,9 @@ export default function UploadImageForm({
   isLoading,
   coverImage,
 }: UploadImageFormProps) {
+  // const fileRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const fileRef = useRef<HTMLInputElement>(null);
+
   const form = useForm<ImageFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -38,59 +40,77 @@ export default function UploadImageForm({
     },
   });
 
-  const onSubmit = (imageData: ImageFormData) => {
-    const formData = new FormData();
-    formData.append("imageFile", imageData.imageFile);
+  const onSubmit = useCallback(
+    (imageData: ImageFormData) => {
+      const formData = new FormData();
+      formData.append("imageFile", imageData.imageFile);
 
-    onSave(formData);
-  };
-
-  // useEffect(() => {
-  //   if(coverImage) {
-  //     onSubmit(form.getValues)
-  //   }
-
-  // }, [coverImage, onSubmit])
+      onSave(formData);
+    },
+    [onSave],
+  );
 
   return (
     <>
-      {coverImage && (
-        <img
-          src={coverImage}
-          className="mb-4 h-[300px] w-full rounded-md object-cover"
-          alt="article cover image"
-        />
-      )}
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="mb-2 flex gap-2"
-        >
+        <form onSubmit={form.handleSubmit(onSubmit)} className="mb-2">
+          <div
+            className="group mb-4 flex h-[300px] cursor-pointer items-center justify-center rounded-md bg-gray-200 hover:bg-gray-300"
+            onClick={() => fileRef.current?.click()}
+          >
+            {coverImage ? (
+              <img
+                src={coverImage}
+                className="h-full w-full rounded-md object-cover"
+                alt="article cover image"
+              />
+            ) : (
+              <div className="flex flex-col items-center">
+                {isLoading ? (
+                  <>
+                    <FileUp size={36} className="animate-bounce" />
+                    <p className="animate-pulse">Uploading...</p>
+                  </>
+                ) : (
+                  <>
+                    <ImagePlus
+                      size={36}
+                      className="text-gray-500 group-hover:text-gray-700"
+                    />
+                    <p className="text-gray-500 group-hover:text-gray-700">
+                      Select cover image
+                    </p>
+                  </>
+                )}
+              </div>
+            )}
+          </div>
+
           <FormField
             name="imageFile"
             control={form.control}
             render={({ field }) => (
               <FormItem>
                 <FormControl>
+                  {/* can use normal input and enable hidden.
+                  hidden doesn ork for shadcn Input that's why class is used. */}
                   <Input
                     type="file"
-                    // className="bg-white"
+                    ref={fileRef}
+                    className="hidden"
                     accept=".jpg, .jpeg, .png"
-                    onChange={(e) =>
-                      field.onChange(e.target.files ? e.target.files[0] : null)
-                    }
+                    onChange={(e) => {
+                      field.onChange(e.target.files ? e.target.files[0] : null);
+                      if (e.target.files && e.target.files.length > 0) {
+                        onSubmit(form.getValues());
+                      }
+                    }}
                   />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-
-          {isLoading ? (
-            <LoadingButton>Uploading...</LoadingButton>
-          ) : (
-            <Button type="submit">Save</Button>
-          )}
         </form>
       </Form>
     </>
